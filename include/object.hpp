8f145e396e2dd33c1cf268bdae85549fa8fe8ed6
João Gerliandro <joaogerliandro@gmail.com>
2022-06-03 13:19:47 -0300

    Object Module + Fullscreen fix

---
+++ include/object.hpp
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+#include <glm/ext.hpp>
+
+std::string obj_dir = "C:\\Users\\joao.martins\\Documents\\GitHub\\sfml-chessboard\\obj\\";
+
+enum OBJECT_TYPE : int32_t
+{
+    CUBE,
+    PYRAMID,
+    ETHER,
+    CONE
+};
+
+typedef struct
+{
+    glm::vec3 world_cords;
+    glm::vec4 obj_color;
+
+    std::vector<glm::vec3> polygon_vertexs;
+
+    OBJECT_TYPE obj_type;
+} Object;
+
+std::vector<glm::vec3> obj_loader(const char* obj_file)
+{
+    //Vertexs
+    std::vector<glm::fvec3> vertexs_positions;
+    //Faces
+    std::vector<GLint>      vertexs_indicies;
+    
+    //Vertexs e temp array
+    std::vector<glm::vec3>  polygon_vertexs;
+
+    std::stringstream str_stream;
+    std::ifstream     in_file(obj_dir + obj_file);
+
+    std::string       line        = "",
+                      line_prefix = "";
+
+    //Aux atributes 
+    GLint     aux_glint = 0;
+    glm::vec2 aux_vec2; 
+    glm::vec3 aux_vec3;
+
+    if(!in_file.is_open())
+        throw "ERRO: Can't open the file !";
+
+    while(std::getline(in_file, line))
+    {
+        str_stream.clear();
+        str_stream.str(line);
+        str_stream >> line_prefix;
+
+        if(line_prefix == "v")
+        {
+            str_stream >> aux_vec3.x >> aux_vec3.y >> aux_vec3.z;
+            vertexs_positions.push_back(aux_vec3);
+        }
+        else if(line_prefix == "f")
+        {
+            int32_t counter = 0;
+            while(str_stream >> aux_glint)
+            {
+                if(counter == 0)
+                    vertexs_indicies.push_back(aux_glint);
+
+                if(str_stream.peek() == '/')
+                {
+                    counter++;
+                    str_stream.ignore(1, '/');
+                }
+                else if(str_stream.peek() == ' ')
+                {
+                    counter++;
+                    str_stream.ignore(1, ' ');
+                }
+
+                if(counter > 2)
+                    counter = 0;
+            }
+        }
+
+        polygon_vertexs.resize(
+            vertexs_indicies.size(),
+            glm::vec3()                    
+        );
+
+        for(size_t i = 0; i < polygon_vertexs.size(); i++)
+            polygon_vertexs[i] = vertexs_positions[vertexs_indicies[i] - 1];
+
+    }
+
+    return polygon_vertexs;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -12,18 +12,12 @@
 #include <glm/ext.hpp>
 
 #include <gl_init.hpp>
+#include <object.hpp>
 
 #define SIDES  360
 #define ANGLE  3.141 * 2.f / SIDES
 #define RADIUS 0.35
 
-enum OBJECT_TYPE : int32_t
-{
-    CUBE,
-    PYRAMID,
-    ETHER,
-    CONE
-};
 typedef struct tile
 {
     glm::vec2 side_x, side_y;
@@ -34,16 +28,6 @@ typedef struct tile
               focused    = false,
               has_object = false;
 }Tile;
-
-typedef struct obj
-{
-    glm::vec3 world_cords;
-    glm::vec4 obj_color;
-
-    std::vector<glm::vec3> polygon_vertexs;
-
-    OBJECT_TYPE obj_type;
-}Object;
 
 typedef struct cam
 {   
@@ -77,78 +61,6 @@ sf::Vector2i mouse_position, window_size;
 sf::Vector2f world_size    , world_position;
 
 sf::RenderWindow window;
-
-std::vector<glm::vec3> obj_loader(const char* obj_path)
-{
-    //Vertexs
-    std::vector<glm::fvec3> vertexs_positions;
-    //Faces
-    std::vector<GLint>      vertexs_indicies;
-    
-    //Vertexs e temp array
-    std::vector<glm::vec3>  polygon_vertexs;
-
-    std::stringstream str_stream;
-    std::ifstream     in_file(obj_path);
-
-    std::string       line        = "",
-                      line_prefix = "";
-
-    //Aux atributes 
-    GLint     aux_glint = 0;
-    glm::vec2 aux_vec2; 
-    glm::vec3 aux_vec3;
-
-    if(!in_file.is_open())
-        throw "ERRO: Can't open the file !";
-
-    while(std::getline(in_file, line))
-    {
-        str_stream.clear();
-        str_stream.str(line);
-        str_stream >> line_prefix;
-
-        if(line_prefix == "v")
-        {
-            str_stream >> aux_vec3.x >> aux_vec3.y >> aux_vec3.z;
-            vertexs_positions.push_back(aux_vec3);
-        }
-        else if(line_prefix == "f")
-        {
-            int32_t counter = 0;
-            while(str_stream >> aux_glint)
-            {
-                if(counter == 0)
-                    vertexs_indicies.push_back(aux_glint);
-
-                if(str_stream.peek() == '/')
-                {
-                    counter++;
-                    str_stream.ignore(1, '/');
-                }
-                else if(str_stream.peek() == ' ')
-                {
-                    counter++;
-                    str_stream.ignore(1, ' ');
-                }
-
-                if(counter > 2)
-                    counter = 0;
-            }
-        }
-
-        polygon_vertexs.resize(
-            vertexs_indicies.size(),
-            glm::vec3()                    
-        );
-
-        for(size_t i = 0; i < polygon_vertexs.size(); i++)
-            polygon_vertexs[i] = vertexs_positions[vertexs_indicies[i] - 1];
-
-    }
-
-    return polygon_vertexs;
-}
 
 void ortho_axis(bool lever)
 {
@@ -551,16 +463,16 @@ int main()
                             switch (aux_obj.obj_type)
                             {
                             case CUBE:
-                                aux_obj.polygon_vertexs = obj_loader("C:\\Users\\joao.martins\\Documents\\GitHub\\sfml-chessboard\\obj\\cube.obj");
+                                aux_obj.polygon_vertexs = obj_loader("cube.obj");
                                 break;
                             case PYRAMID:
-                                aux_obj.polygon_vertexs = obj_loader("C:\\Users\\joao.martins\\Documents\\GitHub\\sfml-chessboard\\obj\\pyramid.obj");
+                                aux_obj.polygon_vertexs = obj_loader("pyramid.obj");
                                 break;
                             case ETHER:
-                                aux_obj.polygon_vertexs = obj_loader("C:\\Users\\joao.martins\\Documents\\GitHub\\sfml-chessboard\\obj\\ether.obj");
+                                aux_obj.polygon_vertexs = obj_loader("ether.obj");
                                 break;
                             case CONE:
-                                aux_obj.polygon_vertexs = obj_loader("C:\\Users\\joao.martins\\Documents\\GitHub\\sfml-chessboard\\obj\\cone.obj");
+                                aux_obj.polygon_vertexs = obj_loader("cone.obj");
                                 break;
                             }
                             
@@ -769,11 +681,15 @@ int main()
                         case sf::Keyboard::F11:
                             fullscreen = !(fullscreen);
                             if(fullscreen)
+                            {
                                 window.create(sf::VideoMode::getDesktopMode(), title_name, sf::Style::Fullscreen, gl_settings());
-
+                                window_size = sf::Vector2i(window.getSize().x, window.getSize().y);
+                            }
                             else
+                            {
                                 window.create(sf::VideoMode::getDesktopMode(), title_name, sf::Style::Default, gl_settings());
-
+                                window_size = sf::Vector2i(window.getSize().x, window.getSize().y);
+                            }
                             break;
                     }
                     break;    
